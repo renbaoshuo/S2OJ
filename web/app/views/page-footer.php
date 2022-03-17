588bf6e3cca960ca2528fa47e7bc89f22d30f256
Baoshuo <i@baoshuo.ren>
2022-03-17 12:42:58 +0800

    fix(web/footer): beian

--- web/app/views/page-footer.php
+++ web/app/views/page-footer.php
@@ -17,8 +17,8 @@
 				</div>
 				
 				<ul class="list-inline"><li class="list-inline-item"><?= UOJConfig::$data['profile']['oj-name'] ?></li></ul>
-				<?php if (UOJConfig::$data['profile']['ICP-license'] != '' && preg_match_all('/(\d+\.?\d+)/', UOJConfig::$data['profile']['ICP-license'], $ICP_number)): ?>
-				<p><a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=<?= $ICP_number[0][0] ?>" style="text-decoration:none;"><img src="http://uoj.ac/pictures/beian.png" /> <?= UOJConfig::$data['profile']['ICP-license'] ?></a></p>
+				<?php if (UOJConfig::$data['profile']['ICP-license'] != ''): ?>
+				<p><a target="_blank" href="https://beian.miit.gov.cn" style="text-decoration:none;"><?= UOJConfig::$data['profile']['ICP-license'] ?></a></p>
 				<?php endif ?>
 				<p><?= UOJLocale::get('server time') ?>: <?= UOJTime::$time_now_str ?> | <a href="https://github.com/renbaoshuo/UOJ-System" target="_blank"><?= UOJLocale::get('opensource project') ?></a></p>
 			</div>
