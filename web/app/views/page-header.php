7fb75835747ce728c6573575c8018761628f8c82
Baoshuo <i@baoshuo.ren>
2022-03-17 21:47:48 +0800

    Revert "fix(web): [CSP] upgrade-insecure-requests"
    This reverts commit c525b570ad1ca4f1db0696b9c98863ce274cd706.

--- web/app/views/page-header.php
+++ web/app/views/page-header.php
@@ -33,7 +33,6 @@
 <html lang="<?= UOJLocale::locale() ?>">
 	<head>
 		<meta charset="utf-8" />
-		<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<?php if (isset($_GET['locale'])): ?>
 		<meta name="robots" content="noindex, nofollow" />
