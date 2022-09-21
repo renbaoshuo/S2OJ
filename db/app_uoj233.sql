2894f377190c2ca5ad739ecf8da8e10245158ec7
Baoshuo <i@baoshuo.ren>
2022-09-21 19:35:55 +0800

    fix: usertype length

--- db/app_uoj233.sql
+++ db/app_uoj233.sql
@@ -777,7 +777,7 @@ CREATE TABLE `user_info` (
   `usergroup` char(1) NOT NULL DEFAULT 'U',
   `username` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `realname` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `usertype` varchar(50) NOT NULL DEFAULT 'student',
+  `usertype` varchar(250) NOT NULL DEFAULT 'student',
   `email` varchar(50) NOT NULL,
   `password` char(32) NOT NULL,
   `svn_password` char(10) NOT NULL,
