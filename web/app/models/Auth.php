354c4177375458e8cdbaf15d78f7d1ffa28e9e17
Baoshuo <i@baoshuo.ren>
2022-10-04 16:44:14 +0800

    fix: c30374732e394d1cce459d7ccbf6a5448e562028

--- web/app/models/Auth.php
+++ web/app/models/Auth.php
@@ -81,7 +81,7 @@ class Auth {
 
 		if ($myUser) {
 			if (!isset($_SESSION['last_login'])) {
-				$_SESSION['last_login'] = $myUser['last_login'];
+				$_SESSION['last_login'] = strtotime($myUser['last_login']);
 			}
 			if ((time() - $_SESSION['last_login']) > 60 * 60 * 24 * 7) {  // 1 week
 				Auth::logout();
