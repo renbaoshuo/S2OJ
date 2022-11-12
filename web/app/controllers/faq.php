1517d453d9b156b528c7bd87275f1c56822db385
Baoshuo <i@baoshuo.ren>
2022-11-12 14:30:02 +0800

    chore(web/faq): heading font size

--- web/app/controllers/faq.php
+++ web/app/controllers/faq.php
@@ -13,12 +13,12 @@ requireLib('mathjax');
 			<article class="card-body">
 				<h1 class="h2 card-title mb-3">常见问题及其解答 (FAQ)</h1>
 
-				<h5 class="mt-4"><?= UOJConfig::$data['profile']['oj-name-short'] ?> 是什么</h5>
+				<h4 class="mt-4"><?= UOJConfig::$data['profile']['oj-name-short'] ?> 是什么</h4>
 				<p class="card-text">
 					见 <a href="https://sjzezoj.com/blogs/1">https://sjzezoj.com/blogs/1</a>。
 				</p>
 
-				<h5 class="mt-4">测评环境</h5>
+				<h4 class="mt-4">测评环境</h4>
 				<p class="card-text">评测机的系统版本是 Ubuntu Server 22.04 LTS。</p>
 				<div class="table-responsive">
 					<table class="table table-bordered text-center align-middle">
@@ -70,7 +70,7 @@ requireLib('mathjax');
 				</div>
 				<p class="card-text">以上信息仅供参考，实际评测环境可能会有变动。</p>
 
-				<h5 class="mt-4">如何上传头像</h5>
+				<h4 class="mt-4">如何上传头像</h4>
 				<p class="card-text">
 					<?= UOJConfig::$data['profile']['oj-name-short'] ?> 不提供头像存储服务。每到一个网站都要上传一个头像挺烦的对不对？我们支持 Gravatar，请使用 Gravatar 吧！Gravatar 是一个全球的头像存储服务，你的头像将会与你的电子邮箱绑定。在各大网站比如各种 Wordpress 还有各种 OJ 比如 Vijos、Contest Hunter 上，只要你电子邮箱填对了，那么你的头像也就立即能显示了！
 				</p>
@@ -81,12 +81,12 @@ requireLib('mathjax');
 					上不去 Gravatar？没关系，我们现在也支持 QQ 头像了！你只需要前往 “更改个人信息” 页面填写自己的 QQ 号，并将 “头像来源” 选为 “QQ” 就可以让你的 QQ 头像显示在 S2OJ 上啦！
 				</p>
 
-				<h5 class="mt-4">递归 10<sup>7</sup> 层怎么没爆栈啊</h5>
+				<h4 class="mt-4">递归 10<sup>7</sup> 层怎么没爆栈啊</h4>
 				<p class="card-text">
 					没错就是这样！除非是特殊情况，<?= UOJConfig::$data['profile']['oj-name-short'] ?> 测评程序时的栈大小与该题的空间限制是相等的！
 				</p>
 
-				<h5 class="mt-4">联系方式</h5>
+				<h4 class="mt-4">联系方式</h4>
 				<p class="card-text">
 					题目相关问题请联系各校区的竞赛教练以及题目管理员。
 				</p>
@@ -95,14 +95,14 @@ requireLib('mathjax');
 					和 <a href="https://sjzezoj.com/user/nekko" class="uoj-username">nekko</a>（<a href="mailto:1139855151@qq.com">1139855151@qq.com</a>）。
 				</p>
 
-				<h5 class="mt-4">开源项目</h5>
+				<h4 class="mt-4">开源项目</h4>
 				<p class="card-text">
 					<?= UOJConfig::$data['profile']['oj-name-short'] ?> 的源代码存放于
 					<a href="https://github.com/renbaoshuo/S2OJ" target="_blank">https://github.com/renbaoshuo/S2OJ</a>。
 					如果你网不太好，打不开 GitHub 的话，也可以点击 <a href="https://git.m.ac/baoshuo/S2OJ" target="_blank">https://git.m.ac/baoshuo/S2OJ</a> 查看哦！这两个仓库的内容是一模一样的。
 				</p>
 
-				<h5 class="mt-4">用户手册</h5>
+				<h4 class="mt-4">用户手册</h4>
 				<p class="card-text">
 					请移步 <a href="https://s2oj.github.io/">S2OJ 使用文档</a>。
 				</p>
