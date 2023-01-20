66acf9dba59fe85e014ffcc571b6c3652a22d41f
Baoshuo <i@baoshuo.ren>
2023-01-20 17:54:49 +0800

    feat: build s2oj-remote-judger image

--- remote_judger/add_judger.sql
+++ remote_judger/add_judger.sql
@@ -1,2 +1,2 @@
 USE `app_uoj233`;
-insert into judger_info (judger_name, password, ip) values ('remote_judger', '_judger_password_', 'uoj-remote-judger');
+insert into judger_info (judger_name, password, ip, display_name, description) values ('remote_judger', '_judger_password_', 'uoj-remote-judger', '远端评测机', '用于桥接远端 OJ 评测机的虚拟评测机。');
