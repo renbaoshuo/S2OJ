3c3ec65551f6631c815a02f23aa2b29337dbca8c
Baoshuo <i@baoshuo.ren>
2023-02-20 11:54:54 +0800

    fix: 8b45b03225b174abd20531a244cfddd0bf96b137

--- web/app/controllers/submission_status_details.php
+++ web/app/controllers/submission_status_details.php
@@ -7,7 +7,7 @@ $res = [];
 foreach ($_GET['get'] as $id) {
 	($submission = UOJSubmission::query($id)) || UOJResponse::page404();
 	$submission->setProblem() || UOJResponse::page404();
-	$submission->userIsSubmitter(Auth::user()) || UOJResponse::page403();
+	// $submission->userIsSubmitter(Auth::user()) || UOJResponse::page403();
 	$submission->userCanView(Auth::user(), ['ensure' => true]);
 	$res[] = [
 		'judged' => $submission->hasJudged(),
